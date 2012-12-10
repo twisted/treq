0c1bf5b13a95a8900e7e132fe6bab56ebab721bd
David Reid <dreid@dreid.org>
2012-12-09 21:51:46 -0800

    Startup and use our own httpbin.

--- treq/test/test_treq_integration.py
+++ treq/test/test_treq_integration.py
@@ -1,12 +1,9 @@
 from twisted.trial.unittest import TestCase
 from twisted.internet.defer import inlineCallbacks
 
-from treq.test.util import DEBUG, is_pypy
+from treq.test.util import DEBUG, HTTPBIN_URL, HTTPSBIN_URL, is_pypy
 
 import treq
-
-HTTPBIN_URL = "http://httpbin.org"
-HTTPSBIN_URL = "https://httpbin.org"
 
 
 @inlineCallbacks
--- treq/test/util.py
+++ treq/test/util.py
@@ -2,5 +2,7 @@ import os
 import platform
 
 DEBUG = os.getenv("TREQ_DEBUG", False) == "true"
+HTTPBIN_URL = os.getenv("HTTPBIN_URL", "http://httpbin.org")
+HTTPSBIN_URL = os.getenv("HTTPSBIN_URL", "https://httpbin.org")
 
 is_pypy = platform.python_implementation() == 'PyPy'

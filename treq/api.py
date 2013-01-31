f124acd26c2c6d64d43df2e9f3f488e1af3d7ef6
David Reid <dreid@dreid.org>
2013-01-31 09:16:11 -0800

    Use a global connection pool by default.

--- treq/api.py
+++ treq/api.py
@@ -1,4 +1,5 @@
 from treq.client import HTTPClient
+from treq._utils import default_pool
 
 
 def head(url, **kwargs):
@@ -86,4 +87,8 @@ def request(method, url, **kwargs):
 #
 
 def _client(*args, **kwargs):
+    kwargs['pool'] = default_pool(kwargs.get('reactor'),
+                                  kwargs.get('pool'),
+                                  kwargs.get('persistent'))
+
     return HTTPClient.with_config(**kwargs)

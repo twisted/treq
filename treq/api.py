57ecf85b610e3f9cbee36c60f665fd01475810c3
Dustin Lacewell <dlacewell@gmail.com>
2014-02-19 11:43:30 -0800

    Don't gzip decode by default. Provide simple helper.

--- treq/api.py
+++ treq/api.py
@@ -102,17 +102,9 @@ def request(method, url, **kwargs):
 #
 
 def _client(*args, **kwargs):
-    kwargs['pool'] = default_pool(kwargs.get('reactor'),
-                                  kwargs.get('pool'),
-                                  kwargs.get('persistent'))
-
     reactor = default_reactor(kwargs.get('reactor'))
-
-    pool = kwargs.get('pool')
-    if not pool:
-        persistent = kwargs.get('persistent', True)
-        pool = HTTPConnectionPool(reactor, persistent=persistent)
-
+    pool = default_pool(reactor,
+                        kwargs.get('pool'),
+                        kwargs.get('persistent'))
     agent = Agent(reactor, pool=pool)
-
     return HTTPClient(agent)

ff57c53bd6fa265dbb18bd2c4f1ea5763bbea48a
L. Daniel Burr <ldanielburr@icloud.com>
2015-01-06 15:37:18 -0600

    Working example of the use of a self-signed certficate with treq.

--- docs/examples/https_server.tac
+++ docs/examples/https_server.tac
@@ -1,13 +1,11 @@
 from twisted.application import internet, service
-from twisted.internet import endpoints, reactor, task
+from twisted.internet import endpoints, reactor
 from twisted.web import server, static
 
 
-endpoint = endpoints.serverFromString(
-    reactor, 'ssl:certKey=./cert.pem:port=8443:privateKey=./private-key.pem'
-)
+endpoint = endpoints.serverFromString(reactor, 'ssl:port=8443')
 rootResource = static.File('.')
-site = server.Site(rootResource, logPath='./http.log')
+site = server.Site(rootResource)
 webService = internet.StreamServerEndpointService(endpoint, site)
 application = service.Application('https_server')
 serviceCollection = service.IServiceCollection(application)
--- docs/examples/using_certs.py
+++ docs/examples/using_certs.py
@@ -1,36 +1,20 @@
-from OpenSSL import SSL
-
-from twisted.internet import defer, endpoints, reactor, ssl, task
-from twisted.python import filepath, log
+from twisted.internet import ssl, task
+from twisted.python import filepath
 from twisted.web import client
 
-import sys
-sys.path.append('.')
+from _utils import print_response
 
 import treq
 
 
-@defer.inlineCallbacks
 def main(reactor, *args):
-    pemFile = filepath.FilePath('cert.pem').getContent()
+    pemFile = filepath.FilePath('server.pem').getContent()
     certificate = ssl.Certificate.loadPEM(pemFile)
     customPolicy = client.BrowserLikePolicyForHTTPS(certificate)
-    response = yield treq.get('https://127.0.0.1:8443/', policy=customPolicy)
-    print response.code
-    defer.returnValue(None)
+
+    d = treq.get('https://localhost:8443/', policy=customPolicy)
+    d.addCallback(print_response)
+    return d
 
 
-def _error(failure):
-    log.msg(str(failure), system='epicFail')
-    if hasattr(failure.value, 'reasons'):
-        reasons = failure.value.reasons
-        response = failure.value.response
-        log.msg(str(response), system='epicFail')
-        for reason in reasons:
-            log.msg(reason.getErrorMessage(), system='epicFail')
-
-
-if __name__ == '__main__':
-    log.startLogging(sys.stdout)
-    d = task.react(main, [])
-    d.addErrback(_error)
+task.react(main, [])

f8c6dd7ef0d980beab39f86506dd8e14a81c7f3f
Glyph <glyph@twistedmatrix.com>
2016-08-31 02:22:57 -0700

    flake8

--- treq/__init__.py
+++ treq/__init__.py
@@ -1,12 +1,12 @@
 from __future__ import absolute_import, division, print_function
 
+from twisted.python.modules import getModule as _getModule
+
 from treq.api import head, get, post, put, patch, delete, request
 from treq.content import collect, content, text_content, json_content
 
 __all__ = ['head', 'get', 'post', 'put', 'patch', 'delete', 'request',
            'collect', 'content', 'text_content', 'json_content']
-
-from twisted.python.modules import getModule as _getModule
 
 __version__ = (
     _getModule(__name__).filePath.sibling("_version").getContent().strip()

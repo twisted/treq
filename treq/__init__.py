9b80d87d21bf833b8f01df33c0dabc1bf4d37845
Glyph <glyph@twistedmatrix.com>
2016-08-31 01:55:43 -0700

    use twisted, instead of pkg_resources, for version

--- treq/__init__.py
+++ treq/__init__.py
@@ -1,11 +1,11 @@
 from __future__ import absolute_import, division, print_function
 
-from pkg_resources import resource_string
-
 from treq.api import head, get, post, put, patch, delete, request
 from treq.content import collect, content, text_content, json_content
 
 __all__ = ['head', 'get', 'post', 'put', 'patch', 'delete', 'request',
            'collect', 'content', 'text_content', 'json_content']
 
-__version__ = resource_string(__name__, "_version").strip()
+from twisted.python.modules import getModule as _getModule
+
+__version__ = _getModule(__name__).filePath.sibling("_version").strip()

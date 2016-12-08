799333d7a5052af7d68f02ef722aa2b6455e9f20
Tom Most <twm@freecog.net>
2016-12-07 18:55:36 -0800

    Require pyOpenSSL 0.15.1

--- setup.py
+++ setup.py
@@ -30,10 +30,9 @@ setup(
         "requests >= 2.1.0",
         "six",
         "Twisted[tls] >= 16.0.0",
+        # Twisted[tls] 16.0.0 only requires 0.13, which doesn't work on Py3.
+        "pyOpenSSL >= 0.15.1",
     ],
-    extras_require={
-        ':python_version > "3.0"': ['pyOpenSSL >= 0.15.1'],
-    },
     package_data={"treq": ["_version"]},
     author="David Reid",
     author_email="dreid@dreid.org",

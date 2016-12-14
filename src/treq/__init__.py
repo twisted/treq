f148124f7c6eaa861371914fa8f90b434a484f5d
Amber Brown (HawkOwl) <hawkowl@atleastfornow.net>
2016-12-14 15:59:47 +1100

    test fixes

--- src/treq/__init__.py
+++ src/treq/__init__.py
@@ -1,13 +1,11 @@
 from __future__ import absolute_import, division, print_function
 
-from twisted.python.modules import getModule as _getModule
+from ._version import __version__
 
 from treq.api import head, get, post, put, patch, delete, request
 from treq.content import collect, content, text_content, json_content
 
+__version__ = __version__.base()
+
 __all__ = ['head', 'get', 'post', 'put', 'patch', 'delete', 'request',
            'collect', 'content', 'text_content', 'json_content']
-
-from ._version import __version__
-
-__version__ = __version__.base()

f5ad4e54e42ffa7b4a7600eea0a331daa55bce42
Amber Brown (HawkOwl) <hawkowl@atleastfornow.net>
2016-12-14 14:52:50 +1100

    add a incremental

---
+++ src/treq/_version.py
@@ -0,0 +1,11 @@
+"""
+Provides Treq version information.
+"""
+
+# This file is auto-generated! Do not edit!
+# Use `python -m incremental.update Treq` to change this file.
+
+from incremental import Version
+
+__version__ = Version('Treq', 16, 12, 0)
+__all__ = ["__version__"]

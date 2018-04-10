6b9179d36a60fa83d8e57f94b4d8a97e0e2c0a0e
Tom Most <twm@freecog.net>
2018-04-09 20:26:25 -0700

    Remove support for Python 3.3
    Sphinx has dropped support for it, breaking our build.

--- setup.py
+++ setup.py
@@ -8,7 +8,6 @@ classifiers = [
     "Framework :: Twisted",
     "Programming Language :: Python",
     "Programming Language :: Python :: 2.7",
-    "Programming Language :: Python :: 3.3",
     "Programming Language :: Python :: 3.4",
     "Programming Language :: Python :: 3.5",
     "Programming Language :: Python :: Implementation :: CPython",

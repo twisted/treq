acce3ce3e16b42b29957e9a52c82a573d2b51856
Thomas Grainger <tagrain@gmail.com>
2019-04-12 15:00:30 +0100

    update download_file to make use of collect flow control

--- docs/examples/download_file.py
+++ docs/examples/download_file.py
@@ -1,13 +1,31 @@
+import io
+import functools
+
 from twisted.internet.task import react
+from twisted.internet import threads, defer
 
 import treq
 
 
-def download_file(reactor, url, destination_filename):
-    destination = open(destination_filename, 'wb')
-    d = treq.get(url, unbuffered=True)
-    d.addCallback(treq.collect, destination.write)
-    d.addBoth(lambda _: destination.close())
-    return d
+def deferToThread(reactor, f, *args, **kwargs):
+    if reactor is None:
+        from twisted.internet import reactor
+    return threads.deferToThreadPool(
+        reactor, reactor.getThreadPool(),
+        f, *args, **kwargs
+    )
+
+
+@defer.inlineCallbacks
+def download_file(reactor, url, dest):
+    response = yield treq.get(url, reactor=reactor, unbuffered=True)
+    f = yield deferToThread(reactor, io.open, dest, 'wb')
+    try:
+        yield treq.collect(
+            response,
+            functools.partial(deferToThread, reactor, f.write),
+        )
+    finally:
+        yield deferToThread(reactor, f.close)
 
 react(download_file, ['http://httpbin.org/get', 'download.txt'])

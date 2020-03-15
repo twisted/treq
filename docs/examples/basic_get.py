1a3df8a304ec3ec6eb52de27dc7b26eb707287b8
Tom Most <twm@freecog.net>
2020-03-15 12:35:51 -0700

    HTTPS Everywhere for httpbin.org URLs

--- docs/examples/basic_get.py
+++ docs/examples/basic_get.py
@@ -5,7 +5,7 @@ import treq
 
 
 def main(reactor, *args):
-    d = treq.get('http://httpbin.org/get')
+    d = treq.get('https://httpbin.org/get')
     d.addCallback(print_response)
     return d
 
--- docs/examples/using_cookies.py
+++ docs/examples/using_cookies.py
@@ -5,14 +5,14 @@ import treq
 
 
 def main(reactor, *args):
-    d = treq.get('http://httpbin.org/cookies/set?hello=world')
+    d = treq.get('https://httpbin.org/cookies/set?hello=world')
 
     def _get_jar(resp):
         jar = resp.cookies()
 
         print('The server set our hello cookie to: {}'.format(jar['hello']))
 
-        return treq.get('http://httpbin.org/cookies', cookies=jar)
+        return treq.get('https://httpbin.org/cookies', cookies=jar)
 
     d.addCallback(_get_jar)
     d.addCallback(print_response)

277041a23baf563aeaa5a258c69552e96dd3d781
Kyle Altendorf <sda@fstab.net>
2020-09-07 16:27:01 -0400

    Add python_requires for 2.7, 3.5+

--- setup.py
+++ setup.py
@@ -27,6 +27,7 @@ if __name__ == "__main__":
         package_dir={"": "src"},
         setup_requires=["incremental"],
         use_incremental=True,
+        python_requires='>=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*,!=3.4.*',
         install_requires=[
             "incremental",
             "requests >= 2.1.0",

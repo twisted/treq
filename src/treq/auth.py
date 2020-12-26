ace1faf79869e81a115cae002867781a1a544d23
Tom Most <twm@freecog.net>
2020-12-26 14:07:56 -0800

    Document treq.auth
    Closes #313.

--- src/treq/auth.py
+++ src/treq/auth.py
@@ -1,3 +1,5 @@
+# Copyright 2012-2020 The treq Authors.
+# See LICENSE for details.
 from __future__ import absolute_import, division, print_function
 
 import base64
@@ -8,6 +10,9 @@ from zope.interface import implementer
 
 
 class UnknownAuthConfig(Exception):
+    """
+    The authentication config provided couldn't be interpreted.
+    """
     def __init__(self, config):
         super(Exception, self).__init__(
             '{0!r} not of a known type.'.format(config))
@@ -15,6 +20,15 @@ class UnknownAuthConfig(Exception):
 
 @implementer(IAgent)
 class _RequestHeaderSettingAgent(object):
+    """
+    Wrap an agent to set request headers
+
+    :ivar _agent: The wrapped agent.
+
+    :ivar _request_headers:
+        Headers to set on each request before forwarding it to the wrapped
+        agent.
+    """
     def __init__(self, agent, request_headers):
         self._agent = agent
         self._request_headers = request_headers
@@ -23,6 +37,7 @@ class _RequestHeaderSettingAgent(object):
         if headers is None:
             headers = self._request_headers
         else:
+            # FIXME: This mutates *headers*
             for header, values in self._request_headers.getAllRawHeaders():
                 headers.setRawHeaders(header, values)
 
@@ -31,6 +46,24 @@ class _RequestHeaderSettingAgent(object):
 
 
 def add_basic_auth(agent, username, password):
+    """
+    Wrap an agent to add HTTP basic authentication
+
+    The returned agent sets the *Authorization* request header according to the
+    basic authentication scheme described in :rfc:`7617`. This header contains
+    the given *username* and *password* in plaintext, and thus should only be
+    used over an encrypted transport (HTTPS).
+
+    Note that the colon (``:``) is used as a delimiter between the *username*
+    and *password*, so if either parameter includes a colon the interpretation
+    of the *Authorization* header is server-defined.
+
+    :param agent: Agent to wrap.
+    :param username: Username as an ASCII string.
+    :param password: Password as an ASCII string.
+
+    :returns: :class:`~twisted.web.iweb.IAgent`
+    """
     creds = base64.b64encode(
         '{0}:{1}'.format(username, password).encode('ascii'))
     return _RequestHeaderSettingAgent(
@@ -39,6 +72,19 @@ def add_basic_auth(agent, username, password):
 
 
 def add_auth(agent, auth_config):
+    """
+    Wrap an agent to perform authentication
+
+    :param agent: Agent to wrap.
+
+    :param auth_config:
+        A ``('username', 'password')`` tuple --- see :func:`add_basic_auth`.
+
+    :returns: :class:`~twisted.web.iweb.IAgent`
+
+    :raises UnknownAuthConfig:
+        When the format *auth_config* isn't supported.
+    """
     if isinstance(auth_config, tuple):
         return add_basic_auth(agent, auth_config[0], auth_config[1])
 

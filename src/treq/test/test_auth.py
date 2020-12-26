a6ffa49f61d699f85e5c485e44541411016ca2cf
Tom Most <twm@freecog.net>
2020-12-25 23:40:08 -0800

    Replace mock use in test_auth

--- src/treq/test/test_auth.py
+++ src/treq/test/test_auth.py
@@ -1,27 +1,25 @@
-import mock
-
-from twisted.trial.unittest import TestCase
-from twisted.web.client import Agent
+from twisted.trial.unittest import SynchronousTestCase
 from twisted.web.http_headers import Headers
 
 from treq.auth import _RequestHeaderSettingAgent, add_auth, UnknownAuthConfig
+from treq._recorder import recorder
 
 
-class RequestHeaderSettingAgentTests(TestCase):
+class RequestHeaderSettingAgentTests(SynchronousTestCase):
     def setUp(self):
-        self.agent = mock.Mock(Agent)
+        self.agent, self.requests = recorder()
 
     def test_sets_headers(self):
         agent = _RequestHeaderSettingAgent(
             self.agent,
-            Headers({b'X-Test-Header': [b'Test-Header-Value']}))
+            Headers({b'X-Test-Header': [b'Test-Header-Value']}),
+        )
 
-        agent.request('method', 'uri')
+        agent.request(b'method', b'uri')
 
-        self.agent.request.assert_called_once_with(
-            'method', 'uri',
-            headers=Headers({b'X-Test-Header': [b'Test-Header-Value']}),
-            bodyProducer=None
+        self.assertEqual(
+            self.requests[0].headers,
+            Headers({b'X-Test-Header': [b'Test-Header-Value']}),
         )
 
     def test_overrides_per_request_headers(self):
@@ -31,45 +29,58 @@ class RequestHeaderSettingAgentTests(TestCase):
         )
 
         agent.request(
-            'method', 'uri',
+            b'method', b'uri',
             Headers({b'X-Test-Header': [b'Unwanted-Value']})
         )
 
-        self.agent.request.assert_called_once_with(
-            'method', 'uri',
-            headers=Headers({b'X-Test-Header': [b'Test-Header-Value']}),
-            bodyProducer=None
+        self.assertEqual(
+            self.requests[0].headers,
+            Headers({b'X-Test-Header': [b'Test-Header-Value']}),
         )
 
 
-class AddAuthTests(TestCase):
-    def setUp(self):
-        self.rhsa_patcher = mock.patch('treq.auth._RequestHeaderSettingAgent')
-        self._RequestHeaderSettingAgent = self.rhsa_patcher.start()
-        self.addCleanup(self.rhsa_patcher.stop)
-
+class AddAuthTests(SynchronousTestCase):
     def test_add_basic_auth(self):
-        agent = mock.Mock()
+        """
+        add_auth() wraps the given agent with one that adds an ``Authorization:
+        Basic ...`` HTTP header that contains the given credentials.
+        """
+        agent, requests = recorder()
+        authAgent = add_auth(agent, ('username', 'password'))
 
-        add_auth(agent, ('username', 'password'))
+        authAgent.request(b'method', b'uri')
 
-        self._RequestHeaderSettingAgent.assert_called_once_with(
-            agent,
+        self.assertEqual(
+            requests[0].headers,
             Headers({b'authorization': [b'Basic dXNlcm5hbWU6cGFzc3dvcmQ=']})
         )
 
     def test_add_basic_auth_huge(self):
-        agent = mock.Mock()
+        """
+        The Authorization header doesn't include linebreaks, even if the
+        credentials are so long that Python's base64 implementation inserts
+        them.
+        """
+        agent, requests = recorder()
         pwd = ('verylongpasswordthatextendsbeyondthepointwheremultiplel'
                'inesaregenerated')
         auth = (b'Basic dXNlcm5hbWU6dmVyeWxvbmdwYXNzd29yZHRoYXRleHRlbmRzY'
                 b'mV5b25kdGhlcG9pbnR3aGVyZW11bHRpcGxlbGluZXNhcmVnZW5lcmF0ZWQ=')
-        add_auth(agent, ('username', pwd))
+        authAgent = add_auth(agent, ('username', pwd))
 
-        self._RequestHeaderSettingAgent.assert_called_once_with(
-            agent,
-            Headers({b'authorization': [auth]}))
+        authAgent.request(b'method', b'uri')
+
+        self.assertEqual(
+            requests[0].headers,
+            Headers({b'authorization': [auth]}),
+        )
 
     def test_add_unknown_auth(self):
-        agent = mock.Mock()
-        self.assertRaises(UnknownAuthConfig, add_auth, agent, mock.Mock())
+        """
+        add_auth() raises UnknownAuthConfig when given anything other than
+        a tuple.
+        """
+        agent, _ = recorder()
+        invalidAuth = 1234
+
+        self.assertRaises(UnknownAuthConfig, add_auth, agent, invalidAuth)

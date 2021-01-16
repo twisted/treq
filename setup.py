4fc36765af2e1f4dc343c6ea295b6af7f96a5a4d
Tom Most <twm@freecog.net>
2021-01-16 13:04:29 -0800

    Update package metadata

--- setup.py
+++ setup.py
@@ -7,8 +7,6 @@ classifiers = [
     "Operating System :: OS Independent",
     "Framework :: Twisted",
     "Programming Language :: Python",
-    "Programming Language :: Python :: 2.7",
-    "Programming Language :: Python :: 3.5",
     "Programming Language :: Python :: 3.6",
     "Programming Language :: Python :: 3.7",
     "Programming Language :: Python :: 3.8",
@@ -28,7 +26,7 @@ if __name__ == "__main__":
         package_dir={"": "src"},
         setup_requires=["incremental"],
         use_incremental=True,
-        python_requires='>=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*,!=3.4.*',
+        python_requires='>=3.5',
         install_requires=[
             "incremental",
             "requests >= 2.1.0",

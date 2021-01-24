f85ed5cfb17d1d9eb1fa34c677afe6270d85fc88
Tom Most <twm@freecog.net>
2021-01-24 14:42:13 -0800

    Update RTD config to use Python 3.7
    This fixes the current build failure.

--- setup.py
+++ setup.py
@@ -42,8 +42,10 @@ if __name__ == "__main__":
                 "mock",
                 "pep8",
                 "pyflakes",
-                "sphinx",
                 "httpbin==0.5.0",
+            ],
+            "docs": [
+                "sphinx>=1.4.8",
             ],
         },
         package_data={"treq": ["_version"]},

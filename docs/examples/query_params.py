6fa84c75943b282f80a011f7beb20072ab6686b6
Tom Most <twm@freecog.net>
2025-02-15 15:21:24 -0800

    Tersify query_params example

--- docs/examples/query_params.py
+++ docs/examples/query_params.py
@@ -8,35 +8,30 @@ async def query_params(reactor):
     resp = await treq.get(
         "https://httpbin.org/get", params=[("foo", "bar"), ("baz", "bax")]
     )
-    content = await resp.text()
-    print(content)
+    print(await resp.text())
 
     print("Single value dictionary")
     resp = await treq.get(
         "https://httpbin.org/get", params={"foo": "bar", "baz": "bax"}
     )
-    content = await resp.text()
-    print(content)
+    print(await resp.text())
 
     print("Multi value dictionary")
     resp = await treq.get(
         "https://httpbin.org/get", params={b"foo": [b"bar", b"baz", b"bax"]}
     )
-    content = await resp.text()
-    print(content)
+    print(await resp.text())
 
     print("Mixed value dictionary")
     resp = await treq.get(
         "https://httpbin.org/get",
         params={"foo": [1, 2, 3], "bax": b"quux", b"bar": "foo"},
     )
-    content = await resp.text()
-    print(content)
+    print(await resp.text())
 
     print("Preserved query parameters")
     resp = await treq.get("https://httpbin.org/get?foo=bar", params={"baz": "bax"})
-    content = await resp.text()
-    print(content)
+    print(await resp.text())
 
 
 react(query_params)

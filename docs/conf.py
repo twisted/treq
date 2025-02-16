e5f3253de9b0e0b67843613de0f7b2868b086873
Tom Most <twm@freecog.net>
2025-02-15 16:35:29 -0800

    Set RTD options

--- docs/conf.py
+++ docs/conf.py
@@ -99,10 +99,13 @@ pygments_style = "sphinx"
 # a list of builtin themes.
 html_theme = "sphinx_rtd_theme"
 
+# https://docs.readthedocs.com/platform/stable/intro/sphinx.html#set-the-canonical-url
+html_baseurl = os.environ.get("READTHEDOCS_CANONICAL_URL", "/")
+
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-# html_theme_options = {}
+html_theme_options = {"flyout_display": "attached"}
 
 # Add any paths that contain custom themes here, relative to this directory.
 # html_theme_path = []
